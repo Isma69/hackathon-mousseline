b71293fc9f0a1a0067cb55090f2db7b563606913
Dervieux <dervieux.ben@gmail.com>
2022-06-29 18:58:04 +0200

    merging

--- hackathon/pages/index.js
+++ hackathon/pages/index.js
@@ -1,12 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../component/Layout/Layout";
-import styles from "../styles/Home.module.css";
+import Learning from "../component/Learning/Learning";
+import style from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Layout>
-      <div>La Chatte</div>
+    <Layout pageTitle="Home page">
+      <div>
+        <h2 className={style.title}>
+          A culture of sharing and shared learning
+        </h2>
+      </div>
+      <Learning />
     </Layout>
   );
 }
