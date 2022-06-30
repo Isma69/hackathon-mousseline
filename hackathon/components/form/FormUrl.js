729ae67ababf5767cc4f5ecea2299f8efffda8b3
Dervieux <dervieux.ben@gmail.com>
2022-06-30 18:41:36 +0200

    c'est super

--- hackathon/components/form/FormUrl.js
+++
@@ -1,3 +0,0 @@
-export default function FormUrl() {
-  return <div>Enter</div>;
-}
--- hackathon/pages/index.js
+++ hackathon/pages/index.js
@@ -1,9 +1,15 @@
-import Layout from '../components/Layout/Layout';
-import CircleChoice from '../components/CircleChoice';
-import style from '../styles/Home.module.css';
-import { useContext } from 'react';
-import { CardsContext } from '../context/CardsContext';
-import CardItem from '../components/cardItem/CardItem';
+import Layout from "../components/Layout/Layout";
+import CircleChoice from "../components/CircleChoice";
+import style from "../styles/Home.module.css";
+import { useContext } from "react";
+import { CardsContext } from "../context/CardsContext";
+import CardItem from "../components/cardItem/CardItem";
+import FormActuality from "../components/form/FormActuality";
+import FormShare from "../components/form/FormShare";
+import FormLearning from "../components/form/FormLearning";
+import FormSource from "../components/form/FormSource";
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
+import Link from "next/link";
 
 export default function Home() {
   const {
@@ -19,6 +25,8 @@ export default function Home() {
 
   console.log(actualityList);
 
+  const ShowForm = () => {};
+
   return (
     <Layout>
       <div className={style.mainWrapper}>
@@ -26,26 +34,41 @@ export default function Home() {
           <div className={style.mainContainer}>
             <CircleChoice />
           </div>
-
-          <div className={`${showActuality ? '' : style.showContent}`}>
+          <div className={`${showActuality ? "" : style.showContent}`}>
+            <Link href="#formActuality">
+              <BsFillArrowDownCircleFill className={style.btnScroll} />
+            </Link>
             {actualityList.map((actuality) => (
               <CardItem card={actuality} key={actuality.id} id={actuality.id} />
             ))}
+            <FormActuality />
           </div>
-          <div className={`${showLearning ? '' : style.showContent}`}>
+          <div className={`${showLearning ? "" : style.showContent}`}>
+            <Link href="#formLearning">
+              <BsFillArrowDownCircleFill className={style.btnScroll} />
+            </Link>
             {learningList.map((learning) => (
               <CardItem card={learning} key={learning.id} id={learning.id} />
             ))}
+            <FormLearning />
           </div>
-          <div className={`${showShare ? '' : style.showContent}`}>
+          <div className={`${showShare ? "" : style.showContent}`}>
+            <Link href="#formShare">
+              <BsFillArrowDownCircleFill className={style.btnScroll} />
+            </Link>
             {shareList.map((share) => (
               <CardItem card={share} key={share.id} id={share.id} />
             ))}
+            <FormShare />
           </div>
-          <div className={`${showSource ? '' : style.showContent}`}>
+          <div className={`${showSource ? "" : style.showContent}`}>
+            <Link href="#formSource">
+              <BsFillArrowDownCircleFill className={style.btnScroll} />
+            </Link>
             {sourceList.map((source) => (
               <CardItem card={source} key={source.id} id={source.id} />
             ))}
+            <FormSource />
           </div>
         </div>
       </div>
