304fc954e1ab4182b378ffedcf1a06ff71a6e43f
CLément <clmentmoreno@hotmail.com>
2022-06-30 10:29:05 +0200

    work finished

--- hackathon/pages/index.js
+++ hackathon/pages/index.js
@@ -1,12 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import Layout from "../component/Layout/Layout";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import CircleChoice from '../component/CircleChoice';
+import Layout from '../component/Layout/Layout';
+import style from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <Layout>
       <div>La Chatte</div>
+      <CircleChoice />
     </Layout>
   );
 }
