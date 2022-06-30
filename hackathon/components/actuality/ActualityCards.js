231e59b1672312e5f22ff7f8c75a52f06ed3ee50
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-06-30 10:34:04 +0200

    update db + props

--- hackathon/components/actuality/ActualityCards.js
+++ hackathon/components/actuality/ActualityCards.js
@@ -1,19 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import style from '../../styles/ComponentsStyle.module.css';
 
-export default function ActualityCards() {
-  // const [actuality, setActuality] = useState({});
-
-  // // useEffet(() => {
-  // //   id &&
-  // //     axios
-  // //       .get(`/api/actuality/${id}`)
-  // //       .then((res) => setActuality(res.data))
-  // //       .catch((err) => {
-  // //         console.error(err.response.data);
-  // //       });
-  // // }, [id, setActuality]);
-
+export default function ActualityCards(actuality) {
   return (
     <>
       <div className={style.mainWrapper}>
@@ -22,31 +10,18 @@ export default function ActualityCards() {
             <div className={style.imageContainer}>
               <img
                 className={style.imageStyle}
-                src="/images/téléchargement.jpeg"
-                alt="erger"
+                src={actuality.urlPicture}
+                alt={actuality.title}
               />
             </div>
             <div className={style.infoStyle}>
-              <h1>Title</h1>
-              <p>agence de ...</p>
-              <p>date</p>
+              <h1>{actuality.title}</h1>
+              <p>{actuality.premise}</p>
+              <p>{actuality.createdAt}</p>
             </div>
           </div>
 
-          <p className={style.textStyle}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-            risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-            nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-            ligula massa, varius a, semper congue, euismod non, mi. Proin
-            porttitor, orci nec nonummy molestie, enim est eleifend mi, non
-            fermentum diam nisl sit amet erat. Duis semper. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-            lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,
-            dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
-            a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
-            molestie, enim est eleifend mi, non fermentum diam nisl sit amet
-            erat. Duis semper.
-          </p>
+          <p className={style.textStyle}>{actuality.content}</p>
         </div>
       </div>
     </>
